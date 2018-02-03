20fd7c2def8258a8d04181d53e57ba45a8131cda
Jeffrey <jeffreyhxu@gmail.com>
2018-02-02 22:34:07 -0800

    All the test cases listed in the spec work. Hallelujah.

--- proj1/server.cpp
+++ proj1/server.cpp
@@ -1,4 +1,4 @@
-#define portno 9008
+#define portno 9009
 
 #include "server.h"
 
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <fstream>
 #include <cstring>
+#include <climits>
 
 
 #include <stdio.h>
@@ -24,18 +25,18 @@ Server::Server() {
 
   n_cli = 0;
 
-  cout << "Server initialized..." << endl << endl;
+  //cout << "Server initialized..." << endl << endl;
 }
 
 Server::~Server() {
-  cout << "Closing..." << endl;
+  //cout << "Closing..." << endl;
 
   close(cli_fd);
   close(sockfd);
 }
 
 void Server::createSocket() {
-  cout << "Socket created..." << endl;
+  //cout << "Socket created..." << endl;
 
   sockfd = socket(AF_INET, SOCK_STREAM, 0);  // create socket
   if (sockfd < 0)
@@ -48,6 +49,7 @@ void Server::createSocket() {
   serv_addr.sin_addr.s_addr = INADDR_ANY;
   serv_addr.sin_port = htons(portno);
 
+  // attach this address info to the socket
   if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
     cerr << "ERROR on binding: " << strerror(errno) <<  endl;
 
@@ -56,26 +58,21 @@ void Server::createSocket() {
 void Server::startListening() {
   listen(sockfd, 5);  // 5 simultaneous connection at most
 
-  socklen_t clilen;    // not entirely sure what this for..
+  socklen_t clilen;
 
   while (1) {
+    // establish a connection and assign it a fd
     cli_fd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
     int n;
-    ostringstream collector;
-    //string collected;
     char buffer[2048];
 
-    //do{
     memset(buffer, 0, 2048);  // reset memory
 
     //read client's message
     n = read(cli_fd, buffer, 2048);
     if (n < 0){ cerr << "ERROR reading from socket: " << strerror(errno) << endl; return;}
     printf("%s", buffer);
-    //collector << buffer;
-    //collected = collector.str();
-    //} while(collected.length() > 0 && collected.substr(collected.length() - 4) != "\r\n\r\n");
     string collected(buffer);
     
     Request req = parseMessage(collected);
@@ -83,57 +80,27 @@ void Server::startListening() {
     char *response = respond(req, resplen);
 
     //reply to client
-    //n = write(cli_fd, "I got your message\n", 19);
     n = write(cli_fd, response, resplen);
     if (n < 0) cerr << "ERROR writing to socket" << endl;
 
     delete response;
 
-    //shutdown(cli_fd, SHUT_WR);  // close connection
-    close(cli_fd);
+    close(cli_fd);  // close connection
   }
 
-  close(sockfd);
-
-  /* ASYNCHRNOUS?
-  socklen_t clilen;    // not entirely sure what this for..
-  cli_fd.resize(n_cli + 1); // creating room for more clients
-  cli_fd[n_cli] = accept(sockfd, (struct sockaddr *) &cli_addrs[n_cli], &clilen);
-  n_cli++;
-
-  // TODO: ASYNCHRONOUS IO USING SELECT? OR POLL? OR FORK?
-
-  if (cli_fd[0] < 0)
-   cerr << "ERROR on accept" << endl;
-
-    int n;
-    char buffer[2048];
-
-    memset(buffer, 0, 2048);  // reset memory
-
-    //read client's message
-    n = read(cli_fd[0], buffer, 2048);
-    if (n < 0) cerr << "ERROR reading from socket" << endl;
-    printf("%s", buffer);
-
-    //reply to client
-    n = write(cli_fd[0], "I got your message\n", 19);
-    if (n < 0) cerr << "ERROR writing to socket" << endl;
-
-    close(cli_fd[0]);  // close connection
-  }
-    */
+  close(sockfd);  // close socket (also handled in destructor)
 
 }
 
 struct Server::Request Server::parseMessage(string buffer) {
   struct Request req;
 
-  cout << "Attempting to parse message..." << endl;
+  //cout << "Attempting to parse message..." << endl;
 
   vector<string> lines;
   string line = "";
 
+  // break request into lines by /r/n
   for (int i = 0; i < 2048; i++) {
     char c = buffer[i];
 
@@ -152,88 +119,81 @@ struct Server::Request Server::parseMessage(string buffer) {
     }
   }
 
-  for (int i = 0; i < lines.size(); i++) {
+  /*for (int i = 0; i < lines.size(); i++) {
     cout << lines[i] << endl;
-  }
+  }*/
 
   if (lines[0].substr(0, 3) == "GET") {
-    cout << "GETting" << endl;
-    req.filePath = lines[0].substr(4, lines[0].length() - 13);
-    cout << "this: " << req.filePath << endl;
+    //cout << "GETting" << endl;
 
+    // extract the requested file, stripping away the HTTP version and the GET
+    req.filePath = lines[0].substr(4, lines[0].length() - 13);
+    //cout << "this: " << req.filePath << endl;
+
+    // we'll need to use ./filename, so we keep the initial slash.
     string newfp = ".";
     newfp += req.filePath;
+
+    // find and replace all instances of %20 with a space
     int space;
-    
     while((space = newfp.find("%20")) != string::npos){
       newfp = newfp.replace(space, 3, " ");
     }
+
+    // extract the file extension, which will be used to establish content type
     int dot = newfp.find_last_of(".");
     req.exten = "";
     for(int i = dot + 1; i < newfp.length(); i++){
-      newfp[i] = tolower(newfp[i]);
-      req.exten += newfp[i];
+      newfp[i] = tolower(newfp[i]); // make the extension lowercase so it's
+      req.exten += newfp[i];        // not case-sensitive
     }
-    
-    /*for (int i = 0; i < req.filePath.length(); i++) {
-      if (i < req.filePath.length() - 2 && req.filePath[i] == '%' && req.filePath[i + 1] == '2' && req.filePath[i + 2] == '0') {
-        newfp += " ";
-        i += 2;
-        continue;
-      }
-
-      newfp += req.filePath[i];
-    }*/
 
     req.filePath = newfp;
-    cout << "Path: " << req.filePath << endl;
+    //cout << "Path: " << req.filePath << endl;
 
 
   }
 
-  cout << "Parsed!" << endl;
+  //cout << "Parsed!" << endl;
 
   return req;
 }
 
 char * Server::respond(struct Request req, int &resplen) {
-  cout << "Responding..." << endl;
-  cout << req.filePath << endl;
+  //cout << "Responding..." << endl;
+  //cout << req.filePath << endl;
 
   ostringstream oss;
 
   string status = "200 OK";
   int len = 0;
+
+  // attempt to open the file requested
   fstream file;
   file.open(req.filePath.c_str());
-  cout << "Opening file..." << endl;
+  //cout << "Opening file..." << endl;
   char *body = new char[0];
   if(file.fail()){
-    cout << "No file" << endl;
+    // It will fail if the file does not exist, triggering a 404
+    //cout << "No file" << endl;
     status = "404 Not Found";
+    delete body;
+    body = new char[36];
+    strcpy(body, "<html><h1>404 Not Found</h1></html>");
+    len = 36;
   }
   else{
-    cout << "File found" << endl;
-    char *bodbuf = new char[1024];
-    char *acc = new char[1024];
-    while(!file.fail()){
-      file.read(bodbuf, 1024);
-      char *newacc = new char[len + 1024];
-      memcpy(newacc, acc, len);
-      delete [] acc;
-      acc = newacc;
-      memcpy(acc + len, bodbuf, 1024);
-      
-      len += 1024;
-    }
-    delete [] bodbuf;
-    len -= 1024;
-    len += file.gcount();
-    body = new char[len];
-    memcpy(body, acc, len);
-    file.close();
+    //cout << "File found" << endl;
+    file.ignore(INT_MAX); // skip to the end of the file
+    len = file.gcount();  // see how far we skipped, that's the file's length
+    file.clear();
+    file.seekg(ios_base::beg);
+    delete body;
+    body = new char[len]; // read the whole file
+    file.read(body, len);
   }
 
+  // the content type depends on the file extension found earlier
   string type = "octet-stream";
   if(req.exten == "html" || req.exten == "htm"){
     type = "text/html";
@@ -241,19 +201,28 @@ char * Server::respond(struct Request req, int &resplen) {
   else if(req.exten == "jpg" || req.exten == "jpeg" || req.exten == "gif"){
     type = "image";
   }
+
+  // The only headers needed are these
   oss << "Content-Length: " << len << "\r\nContent-Type: " << type << "\r\n";
   string headers = oss.str();
 
+  // the status line and headers are combined here
+  // Notice that these use C++ strings because it is easy, but the file is
+  // read into a C string to ensure all bytes are preserved
   oss.str("");
   oss << "HTTP/1.1 " << status << "\r\n" << headers << "\r\n";
   string resphead = oss.str();
 
-  cout << "resphead:\n" << resphead << endl;
+  //cout << "resphead:\n" << resphead << endl;
 
+  // combine the header and body into one C string
   char *response = new char[resphead.length() + len + 1];
   strcpy(response, resphead.c_str());
   memcpy(response + resphead.length(), body, len);
-  resplen = resphead.length() + len;
-  printf("Response:\n%sEnd\n", response);
+  delete body; // the contents have been copied out, so this is not needed
+  resplen = resphead.length() + len; // there may be 0 bytes in a binary file,
+                                     // so we need to keep track of length
+                                     // rather than using strlen
+  //printf("Response:\n%sEnd\n", response);
   return response;
 }
